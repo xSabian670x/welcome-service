752d5b838515ea9ca4b440cf730231bb2971885c
JovielPerkins <jovielperkins@gmail.com>
2018-09-27 14:19:41 -0500

    Driver update

--- src/jm/edu/utech/ap/week5/Driver.java
+++ src/jm/edu/utech/ap/week5/Driver.java
@@ -4,8 +4,7 @@ public class Driver implements Drivable{
 	
 	public void drive(float numberOfMiles)
 	{
-		System.out.println();
-		
+		System.out.println(String.format("Jovi has Driven" + "%.2f miles", numberOfMiles));
 	}
 	
 
